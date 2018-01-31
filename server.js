c7be76a1ea6b3b2c231bdab7c89e49c119295db8
Tony Gannuch <tgannuch@gmail.com>
2018-01-31 14:26:21 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use((request, response, next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${request.method} ${request.url}`;
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if (err) {
+			console.log('unable to append server.log');
+		}
+	});
+	next();
+});
+
+// app.use((request, response, next) => {
+// 	response.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+app.get('/', (request, response) => {
+	response.render('home.hbs', {
+		pageTitle: 'Home Page',
+		welcomeMessage: 'Welcome to having no job!',
+	});
+});
+
+app.get('/about', (request, response) => {
+	response.render('about.hbs', {
+		pageTitle: 'About Page',
+	});
+});
+
+
+app.get('/bad', (request, response) => {
+	response.send({
+		errorMessage: 'bad request',
+		why: 'why did you do this'
+	});
+});
+
+app.listen(3000, () => {
+	console.log('server is up on port 3000');
+});
